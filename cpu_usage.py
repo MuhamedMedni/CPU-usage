b04f7f8375c51c2f5e5f73096494f1d0172b4308
Muhammed Medni <32286325+MuhamedMedni@users.noreply.github.com>
2021-10-15 11:59:58 +0200

    Update cpu_usage.py
    Updating cpu_usage.py

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-import psutil, os ,sys
 
-pstotal = 0.0
-for proc in psutil.process_iter():
-    ps_name = proc.name()
-    ps_memory = proc.memory_info().rss
-    #/ 2**20
-    ps_pids = proc.pid
+import psutil, os, sys
+#import time, schedule
 
-    pstotal += float(ps_memory)
-print('Memory:', pstotal)
+changed_memory_m = 0.0
+changed_memory_g = 0.0
 
 Input_memory = (sys.argv[1])
 Input_minute = (sys.argv[2])
@@ -17,18 +12,33 @@ Input_minute = (sys.argv[2])
 memory_number = Input_memory[:-1]
 memory_unit = Input_memory[-1]
 
-if memory_unit == 'M' or memory_unit == 'm':
-    changed_memory_m = (float(pstotal) / 2**20)
-    print(changed_memory_m)
+for proc in psutil.process_iter():
+    ps_name = proc.name()
+    ps_memory = proc.memory_info().rss
+    ps_pids = proc.pid
 
-if memory_unit == 'G' or memory_unit == 'g':
-    changed_memory_g = (float(pstotal) / 2**30)
-    print(changed_memory_g)
- 
+    if memory_unit == 'M' or memory_unit == 'm':
+        changed_memory_m = (float(ps_memory) / 2**20)
+        if float(changed_memory_m) > (float(memory_number)):
+            print(ps_name, changed_memory_m)
+            f = open("/tmp/cpu_usage_rapport.txt", "a")
+#            f.write("WARNING : You're using CPU memory more than )
+            f.write(str(ps_pids)+" ")
+            f.write(str(ps_name)+" ")
+            f.write(str(ps_memory))
+            f.write("\n")
+
+    if memory_unit == 'G' or memory_unit == 'g':
+        changed_memory_g = (float(ps_memory) / 2**30)
+        if float(changed_memory_g) > (float(memory_number)):
+            print(ps_name, changed_memory_g)
+    
+#/tep/sayhello.txt
+#schedule.every().minutes.do()
 #=====================have to start from this part. 14/10 seongeun 
 
-    if int(changed_memory_m) > int(memory_number):
-        print("Stop")
+#    if int(changed_memory_m) > int(memory_number):
+#        print("Stop")
 
 
 #====================I think I can use it for .txt to show the list. so I just left it.
